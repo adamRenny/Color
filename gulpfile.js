ec76825d3e598fe00752ab9494443d50b836afc2
Adam Ranfelt <adamRenny@gmail.com>
2014-07-05 00:13:03 -0500

    Adds complexity reports
    * Adds report task
    * Adds plato to gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,34 @@
 var path = require('path');
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var plato = require('gulp-plato');
+var fs = require('fs');
+var jshintOpts = JSON.parse(fs.readFileSync(path.join(__dirname, '.jshintrc'), 'utf8'));
 
 var SRC = path.join(__dirname, 'src');
 var TEST = path.join(__dirname, 'test');
 var DEST = path.join(__dirname, 'dist');
+var REPORTS = path.join(__dirname, 'reports');
 
 gulp.task('dist', function() {
-    gulp.src([path.join(SRC, 'Color.js')])
+    gulp.src([path.join(SRC, '*.js')])
         .pipe(uglify({
             preserveComments: 'some'
         }))
         .pipe(gulp.dest(DEST));
+});
+
+gulp.task('report', function() {
+    gulp.src([path.join(SRC, '*.js')])
+        .pipe(plato(
+            path.join(REPORTS, 'complexity'),
+            {
+                jshint: jshintOpts,
+                complexity: {
+                    silent: true
+                }
+            })
+        );
 });
 
 gulp.task('default', ['dist']);
